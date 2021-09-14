b9c7ba6dcb4c802a424412b3c4c5826c8d781a39
MM87 <71704916+MM879487@users.noreply.github.com>
2021-09-14 21:03:18 +0800

    Delete google.js

--- google.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keydown", event => {
-    if(event.key == "Enter"){
-        window.open("https://www.youtube.com/watch?v=34Ig3X59_qA");
-        location.reload();
-    }
-});
