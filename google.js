1a16a621154c78709066180affcf1c904ae0e1e0
MM87 <71704916+MM879487@users.noreply.github.com>
2021-09-14 20:53:34 +0800

    Add files via upload

---
+++ google.js
@@ -0,0 +1,6 @@
+document.addEventListener("keydown", event => {
+    if(event.key == "Enter"){
+        window.open("https://www.youtube.com/watch?v=34Ig3X59_qA");
+        location.reload();
+    }
+});
